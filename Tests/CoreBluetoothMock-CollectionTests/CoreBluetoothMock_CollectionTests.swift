bf722940551a34e85d73869e9888b4b7dc157302
Nick Kibysh <nick.kibysh@gmail.com>
2023-08-02 15:07:23 +0200

    Package init

---
+++ Tests/CoreBluetoothMock-CollectionTests/CoreBluetoothMock_CollectionTests.swift
@@ -0,0 +1,12 @@
+import XCTest
+@testable import CoreBluetoothMock_Collection
+
+final class CoreBluetoothMock_CollectionTests: XCTestCase {
+    func testExample() throws {
+        // XCTest Documenation
+        // https://developer.apple.com/documentation/xctest
+
+        // Defining Test Cases and Test Methods
+        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    }
+}
